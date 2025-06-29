cb94080f7dc23af5e4229f0bd6de6dc6ee7b6ab3
Arnob0212 <chakrabortyarnob788@gmail.com>
2025-06-29 17:57:38 +0530

    Update Return_Data_from_a_Stored_Procedure.sql

--- 6361882_Week2/6361882_Week2_Advanced_SQL/4_SQL_Exercise_Stored_Procedure/Exercise_5_Return_Data_from_a_Stored_Procedure/Code/Return_Data_from_a_Stored_Procedure.sql
+++ 6361882_Week2/6361882_Week2_Advanced_SQL/4_SQL_Exercise_Stored_Procedure/Exercise_5_Return_Data_from_a_Stored_Procedure/Code/Return_Data_from_a_Stored_Procedure.sql
@@ -1,3 +1,12 @@
+CREATE PROCEDURE sp_CountEmployeesByDepartment
+    @DeptID INT
+AS
+BEGIN
+    SELECT 
+        COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = @DeptID;
+END;
  
 
 EXEC sp_CountEmployeesByDepartment @DeptID = 3;
